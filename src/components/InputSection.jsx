527d344a56f464f1a253d53a7acc3e9449559f8c
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-13 17:37:43 +0200

    ad result section

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -2,51 +2,74 @@ import React from 'react'
 
 import { useState } from 'react'
 import ListItems from './ListItems'
+import Result from './Result'
 const InputSection = () => {
 
 
     const [ingredient, setIngredient] = useState([])
+    const [isshown, setIsshow] = useState(false)
 
-     
+
 
     const handlerSubmit = (e) => {
         e.preventDefault()
         const form = e.target
         const ingredientValue = form.ingredient.value
-       if (ingredientValue.trim() !== '') {
-        setIngredient([...ingredient, ingredientValue])
-       }    
+        if (ingredientValue.trim() !== '') {
+            setIngredient([...ingredient, ingredientValue])
+        }
         form.reset()
     }
-    
-
-  return (
-    <>
-    
-        <main className=' p-5'> 
-            <form action=""
-            onSubmit={handlerSubmit}
-             className=' flex gap-[12px] p justify-center items-center '>
-                <input
-                 type="text" 
-                 name="ingredient" 
-                 placeholder=' e.g Agatogo' 
-                 className=' p-3 rounded-md  border-gray-500 border-2' />
-
-                 <button
-                 type='submit' 
-                 className=' p-3 rounded-md bg-blue-500 text-white'>+Add ingredient
-                 
-                 </button>
-
-            </form>
-
-        </main>
-        <ListItems ingredients={ingredient} />
-    </>
 
 
-  )
+    return (
+        <>
+
+            <main className=' p-5'>
+                <form action=""
+                    onSubmit={handlerSubmit}
+                    className=' flex gap-[12px] p justify-center items-center '>
+                    <input
+                        type="text"
+                        name="ingredient"
+                        placeholder=' e.g Agatogo'
+                        className=' p-3 rounded-md  border-gray-500 border-2' />
+
+                    <button
+                        type='submit'
+                        className=' p-3 rounded-md bg-blue-500 text-white'>+Add ingredient
+
+                    </button>
+
+                </form>
+
+            </main>
+            <ListItems ingredients={ingredient} setIngredients={setIngredient} />
+
+            {ingredient.length >3? (
+                <div className=' max-w-2xl mx-auto mt-10 bg-white shadow-xl rounded-2xl p-8 border border-gray-100 flex items-center justify-between'>
+
+                    <div className=' flex flex-col gap-2.5'>
+                        <h3 className=' text-2xl font-bold'>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button className=' bg-blue-500 p-3 rounded-[13px] text-white font-medium' onClick={() => {
+                        setIsshow(!isshown)
+                    }}>Get a recipe</button>
+
+
+                </div>
+            ) : null}
+
+
+            {isshown?(
+                <Result></Result>
+            ):null}
+
+        </>
+
+
+    )
 }
 
 export default InputSection
--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,4 +1,13 @@
-function ListItems({ ingredients }) {
+function ListItems({ ingredients, setIngredients }) {
+
+    const deleteIngredient = (indexToDelete) => {
+        setIngredients(prevIngredients =>
+            prevIngredients.filter(
+                (_, index) => index !== indexToDelete
+            )
+        );
+    };
+
     return (
         <div className="max-w-2xl mx-auto mt-10 bg-white shadow-xl rounded-2xl p-8 border border-gray-100">
 
@@ -9,18 +18,20 @@ function ListItems({ ingredients }) {
             <ul className="space-y-3">
                 {ingredients.map((ingredient, index) => (
                     <li
-                        key={index}
-                        className="flex items-center gap-3 text-lg text-gray-700 bg-gray-50 hover:bg-green-50 transition-colors duration-200 px-4 py-3 rounded-lg shadow-sm"
+                        key={ingredient}
+                        onClick={() => deleteIngredient(index)}
+                        className="flex items-center gap-3 text-lg text-gray-700 bg-gray-50 hover:bg-red-50 cursor-pointer transition-colors duration-200 px-4 py-3 rounded-lg shadow-sm"
                     >
-                        <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+                        <span className="w-2 h-2 rounded-full bg-black"></span>
                         {ingredient}
                     </li>
                 ))}
             </ul>
 
             {ingredients.length === 0 && (
-                <h1 className="text-gray-500 italic mt-4 text-2xl">No ingredients added yet. Start by adding some!</h1>
-
+                <h2 className="text-gray-500 italic mt-4 text-xl">
+                    No ingredients added yet. Start by adding some!
+                </h2>
             )}
         </div>
     );
